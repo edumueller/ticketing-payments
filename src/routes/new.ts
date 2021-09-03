ae54fae85c9aba35b4300f5c74c67e0de9649b01
edumueller <eduardo.nicastro@icloud.com>
2021-09-03 16:06:14 -0300

    add stripe payment

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -8,6 +8,7 @@ import {
   NotAuthorizedError,
   OrderStatus,
 } from '@devneering/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -30,6 +31,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
